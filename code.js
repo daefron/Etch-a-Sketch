b3bd86e77ce1b36ff53859a2aa81c04b5a20a281
Daefron <thomas_evans@outlook.com>
2023-05-11 16:10:06 +1000

    Add grid-size change logic

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 const main = document.getElementById('main');
-let input = 100;
+let input = document.getElementById('change').value;
+console.log(input);
 let width = "1 0 " + (100 / input) + "%";
 let r = (input * input);
 while (r > 0) {
@@ -10,7 +11,23 @@ while (r > 0) {
     --r;
 }
 const change = document.getElementById('change');
-change.addEventListener('click', () => {
-    input = prompt("grid width?");
+change.addEventListener('change', () => {
+    changeSize();
 });
-//flex-basis needs to be 100% / width rounded down
+function changeSize() {
+    const box = document.querySelectorAll('.box');
+    box.forEach(box => {
+        main.removeChild(box);
+    });
+    let input = document.getElementById('change').value;
+    console.log(input);
+    let width = "1 0 " + (100 / input) + "%";
+    let r = (input * input);
+    while (r > 0) {
+        const box = document.createElement('div');
+        box.classList.add('box');       
+        box.style.flex = width;
+        main.appendChild(box);
+        --r;
+    }
+}
